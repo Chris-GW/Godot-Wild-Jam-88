78bfd5463530707b35f960aed3e0f476c040011e
moriKama <morikamaconnect@gmail.com>
2025-12-16 13:31:35 -0500

    first aid kit healing working

--- first_aid_kit/first_aid_kit.gd
+++ first_aid_kit/first_aid_kit.gd
@@ -9,8 +9,14 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		try_pickup(body)
 
+	elif body.is_in_group("player"):
+		print("entering anchor")
+		# TODO: the body is the playercontroller, the parent is statemachine
+		var player = body.get_parent()
+		if player is PlayerStateMachine: try_pickup(player)
 
-func try_pickup(player: Player) -> void:
+
+func try_pickup(player) -> void:
 	if player.health < player.max_health:
 		player.change_health(+heal_amount)
 		queue_free()
