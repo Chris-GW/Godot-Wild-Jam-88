74992b764f18309b0b89f00268ab7de699b63169
Chris-GW <Chris-GW@users.noreply.github.com>
2025-12-20 00:00:15 +0100

    fix level_select_menu

--- ui/level_select_menu/level_select_menu.gd
+++ ui/level_select_menu/level_select_menu.gd
@@ -11,41 +11,39 @@ const LEVEL_RESOURCES = [
 @onready var level_title: Label = %LevelTitle
 @onready var level_text: RichTextLabel = %LevelText
 
-var select_level_button_group := ButtonGroup.new()
-
 
 func _ready() -> void:
 	_build_level_buttons()
+	var level_idx = clampi(Global.unlocked_levels, 0, level_buttons.get_child_count() - 1)
+	var level_button: = level_buttons.get_child(level_idx)
+	_on_level_selected.call_deferred(level_button.level_resource)
 
 
 func _build_level_buttons() -> void:
 	for child in level_buttons.get_children():
 		level_buttons.remove_child(child)
 		child.queue_free()
-	select_level_button_group = ButtonGroup.new()
 	
 	for level_resouce: LevelResource in LEVEL_RESOURCES:
 		var level_button: LevelSelectButton = LEVEL_SELECT_BUTTON.instantiate()
 		level_button.level_resource = level_resouce
-		level_button.button_group = select_level_button_group
 		level_button.level_selected.connect(self._on_level_selected)
 		level_buttons.add_child(level_button)
-		
-		if level_resouce.index == Global.unlocked_levels:
-			_on_level_selected.call_deferred(level_resouce)
-			level_button.set_pressed.call_deferred(true)
 
 
 func _on_level_selected(level_resource: LevelResource) -> void:
 	level_title.text = level_resource.name
 	level_text.text = level_resource.text
+	for child in level_buttons.get_children():
+		child.set_pressed(child.level_resource.index == level_resource.index)
 
 
 func _on_start_button_pressed() -> void:
-	var pressed_level_button: LevelSelectButton = select_level_button_group.get_pressed_button()
-	if pressed_level_button:
-		var level_resouce := pressed_level_button.level_resource
-		get_tree().change_scene_to_file(level_resouce.scene_path)
+	for child in level_buttons.get_children():
+		if child is LevelSelectButton and child.is_pressed():
+			var level_resouce: LevelResource = child.level_resource
+			get_tree().change_scene_to_file(level_resouce.scene_path)
+			return
 
 
 func _on_back_button_pressed() -> void:
